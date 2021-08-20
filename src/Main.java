ea3a0f7f7584dd9136ec4c2d1edf2fa306ec255c
PadNim14 <npadmana@purdue.edu>
2021-08-20 18:21:32 -0400

    Added user input to main method

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
+
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello, World");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter two numbers.");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("Sum: " + (a + b));
     }
 }
